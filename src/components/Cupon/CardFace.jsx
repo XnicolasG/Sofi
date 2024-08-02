c91855056aae33aa6490c93f3a08b660211ed1d4
Nicolás García <nicolasg.ngl@gmail.com>
2024-08-02 12:37:04 -0500

    FlipImageCard styles updated

--- src/components/Cupon/CardFace.jsx
+++ src/components/Cupon/CardFace.jsx
@@ -1,13 +1,18 @@
 import { FlipImageCard } from "./FlipImageCard";
 
 export const CardFace = ({ title, content, buttonText, onFlip, backgroundImage, backgroundGradient,flipped }) =>{ 
-   
+  const imageSrc = {
+    frontImg: "/img/me-cupon.jpg",
+    altFront: 'front',
+    backImg: "/img/Me-cupon-back.jpg",
+    altBack: 'back',
+  };
     
     return(
     <div className={`w-full h-full flex flex-col ${backgroundGradient || backgroundImage} bg-cover bg-center p-6 text-slate-100`}>
       <h1 className="text-center font-semibold text-4xl mb-4 bg-gradient-to-r from-indigo-600 to-red-700 bg-clip-text text-transparent">{title}</h1>
       {content && <p className="text-md font-semibold mb-4 bg-black/60 p-1 rounded">{content}</p>}
-      {flipped && <FlipImageCard  />}
+      {flipped && <FlipImageCard imageSrc={imageSrc}  />}
       <button
         onClick={onFlip}
         className="mt-auto w-full max-w-xs mx-auto py-2 text-center font-semibold text-white rounded-xl bg-indigo-500 transition-all shadow-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
--- src/components/Cupon/FlipImageCard.jsx
+++ src/components/Cupon/FlipImageCard.jsx
@@ -1,35 +1,23 @@
-import React from 'react'
 
-export const FlipImageCard = () => {
-  const [state, setState] = useState([
-    {msg: 'hola'},
-    {msg2: 'chao'},
-    {cat: 'yuyito'}
-  ])
+import './FlipImageCard.css'
 
-    const imageSrc = {
-        frontImg: "/img/me-cupon.jpg",
-        altFront: 'front',
-        backImg: "/img/Me-cupon-back.jpg",
-        altBack: 'back',
-    };
+export const FlipImageCard = ({imageSrc}) => {
+
   return (
-    <div className="w-36 h-44 mx-auto relative [perspective:1000px] hover:[transform:rotateY(180deg)] transition-transform duration-500">
-      <div className="w-full h-full relative [transform-style:preserve-3d]">
-        {/* Cara frontal */}
-        <div className="absolute w-full h-full backface-hidden">
-          <img 
-            src={imageSrc.frontImg} 
-            alt={imageSrc.altFront} 
-            className="w-full h-full object-cover rounded shadow-lg"
+    <div className="photo-card mx-auto my-auto ">
+      <div className="photo-card__inner rounded-lg shadow-md">
+        <div className="photo-card__front">
+          <img
+            src={imageSrc.frontImg}
+            alt={imageSrc.altFront}
+            className="w-full h-full object-cover"
           />
         </div>
-        {/* Cara trasera */}
-        <div className="absolute w-full h-full [transform:rotateY(180deg)] backface-hidden">
-          <img 
-            src={imageSrc.backImg} 
-            alt={imageSrc.altBack} 
-            className="w-full h-full object-cover rounded shadow-lg"
+        <div className="photo-card__back">
+          <img
+            src={imageSrc.backImg}
+            alt={imageSrc.altBack}
+            className="w-full h-full object-cover"
           />
         </div>
       </div>
