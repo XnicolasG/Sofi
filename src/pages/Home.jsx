1e564c5ed307cf1e9b3e37e0d2bd2a96135abdfb
Nicolás García <nicolasg.ngl@gmail.com>
2024-08-02 16:34:34 -0500

    updated title in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Letter } from '../components/Home/Letter'
 import { CountDown } from '../components/Home/CountDown'
+import { Title } from '../components/Home/Title'
 
 export const Home = () => {
     const [message, setMessage] = useState('♥️ ♥️ ♥️')
@@ -24,17 +25,18 @@ export const Home = () => {
                 <img className='w-full drop-shadow-xl group-hover:scale-110 transition-all duration-200' src="img/Main.png" alt="main-sofi" />
             </section>
             <h2
-                className='absolute top-4 text-5xl w-full text-sky-950 text-center  font-playground'>
-                Contando los minutos
+                className='absolute top-4 p-2 text-7xl w-full bg-gradient-to-r from-red-400 to-red-800 text-center bg-clip-text text-transparent  font-playground'>
+                {/* Contando los minutos */}
+                <Title />
             </h2>
             <Letter text={message} />
             <CountDown targetDate={targetDate} />
             <aside className='flex w-2/3 justify-between md:justify-around mt-10'>
                 <a
-                    className='w-1/3 md:w-1/4 text-stone-200 text-xl text-center rounded-2xl py-1 font-semibold shadow-lg bg-indigo-400 transition-all duration-100 hover:-translate-y-2 hover:bg-indigo-600'
+                    className='w-1/2 md:w-1/4 text-stone-200 text-xl text-center rounded-2xl py-1 font-semibold shadow-lg bg-indigo-400 transition-all duration-100 hover:-translate-y-2 hover:bg-indigo-600'
                     href="/cupon">Cupon</a>
                 <a
-                    className='w-1/3 md:w-1/4 text-stone-200 text-xl text-center rounded-2xl py-1 font-semibold shadow-lg bg-indigo-400 transition-all duration-100 hover:-translate-y-2 hover:bg-indigo-600'
+                    className='w-1/2 md:w-1/4 text-stone-200 text-xl text-center rounded-2xl py-1 font-semibold shadow-lg bg-indigo-400 transition-all duration-100 hover:-translate-y-2 hover:bg-indigo-600'
                     href="/galeria">Galleria</a>
             </aside>
         </main>
